d517f6d5fbc7f5176366ae7507f2ea905432da58
concettacrispin <116707229+concettacrispin@users.noreply.github.com>
2024-03-12 14:09:58 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
@@ -12,11 +12,10 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
-
-pool.getConnection((err, conn) => {
+db.getConnection((err, conn) => {
     if (err) console.error(err);
     console.log("Connected successfully");
     if (conn) conn.release();
 });
 
-export default pool;
+export default db;
