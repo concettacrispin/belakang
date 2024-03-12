f846e2cb60111cb069825afe05e30f98105ca003
concettacrispin <116707229+concettacrispin@users.noreply.github.com>
2024-03-12 14:02:48 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,23 +1,15 @@
 // Database.js
-import mysql from 'mysql2/promise';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
+const db = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-db.getConnection((err, conn) => {
-    if (err) console.error(err);
-    console.log("Connected successfully");
-    if (conn) conn.release();
 });
 
 export default db;
